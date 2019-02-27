be3e542f9decb7dbf6b632e0c74fc623a35c5efd
Arun Gupta <arun6445@gmail.com>
2019-02-27 13:42:18 +1100

    Added code to read SHA from the environment variable

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const express = require('express');
-
+const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config()
 // Constants
 const PORT = 8080;
 const HOST = '127.0.0.1';
@@ -12,12 +14,13 @@ app.get('/', (req, resp) => {
   resp.send('Hello world\n');
 });
 
-status_response = {
+var obj = JSON.parse(fs.readFileSync('src/metadata.json', 'utf8'))
+var status_response = {
     "myapplication": [
         {
-        "version": "1.0",
-        "description": "pre-interview technical test",
-        "lastcommitsha": "abc57858585"
+        "version": obj.version,
+        "description": obj.description,
+        "lastcommitsha": process.env.SHA
         }
     ]
    }
